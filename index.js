704c9d65f9f3821837415c1988bed5004094d1ef
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2022-07-13 10:32:35 +0430

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const authRoute = require("./routes/auth");
+
+const app = express();
+
+dotenv.config();
+// app.use(express.json);
+
+app.use(express.bodyParser({limit: '50mb'}));
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//* DataBase
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB ..."))
+  .catch((err) => console.log(err));
+
+//* Route
+app.use("/api/auth", authRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running ...");
+});
